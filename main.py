5441fcfd952044fd0419d4a01e028a41bb7c33ac
bliack-swan <61846791+bliack-swan@users.noreply.github.com>
2021-06-15 11:10:32 +0400

    End body of program

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-temp = 0
+temp_s = False
 s = 0
 ss = 0
 
@@ -6,17 +6,16 @@ print("This program get the word, and say you how many litters \"s\" and \"ss\" 
 word = list(input('word: '))
 for x in word:
     if x == 's':
-        if temp == 's':
+        s+=1
+        if temp_s:
             ss+=1
+            temp_s = False
         else:
-            s+=1
-            temp = 's'
+            temp_s = True
     else:
+        temp_s = False
         continue
 
-
-
-
-print(word)
+print(s,ss)
 
 
