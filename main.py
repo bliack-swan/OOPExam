1b8c87ce6a6db318380776e5a537cbdb9891dfa2
bliack-swan <61846791+bliack-swan@users.noreply.github.com>
2021-06-15 11:05:50 +0400

    Add a body for program

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+temp = 0
+s = 0
+ss = 0
 
+print("This program get the word, and say you how many litters \"s\" and \"ss\" in word")
+word = list(input('word: '))
+for x in word:
+    if x == 's':
+        if temp == 's':
+            ss+=1
+        else:
+            s+=1
+            temp = 's'
+    else:
+        continue
+
+
+
+
+print(word)
 
 
